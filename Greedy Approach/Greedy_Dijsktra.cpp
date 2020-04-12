262f8fa06966eb8fb33f420082b8d170743598b9
aksuman055 <aksuman055@gmail.com>
2020-04-12 14:39:16 +0530

    Dijsktra's Algorith

--- Greedy Approach/Greedy_Dijsktra.cpp
+++ Greedy Approach/Greedy_Dijsktra.cpp
@@ -1,22 +1,74 @@
-//Dynamic Programming
+//Greedy Approach
 
 //Author: Abhishek Kumar Suman
 //Roll number:181210002
 //Branch: CSE 2nd year
 
-//Given two strings str1 and str2 and below operations that can performed on str1. Find
-//minimum number of edits(operations) required to convert ‘str1’ into ‘str2’.
+//Implementation of Dijisktra's Algorithm.
 
-//Valid operations are:
-//          1.Replace
-//          2.Remove
-//          3.Insert
+//Time complexity: O(V^2) where, V is the number of vertices of the graph.
 
-//Input : str1 = "sunday", str2 = "saturday"
-//Output : 3
-//The above input requires three operations. Bascially, we need to convert "un" to "atur", so we can
-//replace n with r and insert "a" and "t".
+//The program is for adjacency matrix representation of the graph.
+#include <bits/stdc++.h>
+using namespace std;
 
-//Time complexity:Since we are iterating through all the characters of all the strings, so time complexity is O(NM),
-//where N is the length of str1
-//      M is the length of str2.
+#define V 9 //size of graph
+
+int minDistance(int dist[], bool sptSet[])
+{
+
+    int min = INT_MAX, min_index;
+    for (int v = 0; v < V; v++)
+        if (sptSet[v] == false && dist[v] <= min)
+            min = dist[v], min_index = v;
+    return min_index;
+}
+
+int printSolution(int dist[], int n)
+{
+    cout << "Vertex Distance from Source" << endl;
+    for (int i = 0; i < V; i++)
+        cout << i << " " << dist[i] << endl;
+}
+
+void dijkstra(int graph[V][V], int src)
+{
+    int dist[V];
+    bool sptSet[V];
+
+    for (int i = 0; i < V; i++)
+        dist[i] = INT_MAX, sptSet[i] = false;
+    // Distance of source vertex from itself is always 0
+    dist[src] = 0;
+    // Find shortest path for all vertices
+    for (int count = 0; count < V - 1; count++)
+    {
+        int u = minDistance(dist, sptSet);
+
+        sptSet[u] = true;
+
+        for (int v = 0; v < V; v++)
+
+            if (!sptSet[v] && graph[u][v] && dist[u] != INT_MAX && dist[u] + graph[u][v] < dist[v])
+                dist[v] = dist[u] + graph[u][v];
+    }
+
+    printSolution(dist, V);
+}
+
+int main()
+{
+    int graph[V][V] = {{0, 4, 0, 0, 0, 0, 0, 8, 0},
+                       {4, 0, 8, 0, 0, 0, 0, 11, 0},
+                       {0, 8, 0, 7, 0, 4, 0, 0, 2},
+                       {0, 0, 7, 0, 9, 14, 0, 0, 0},
+                       {0, 0, 0, 9, 0, 10, 0, 0, 0},
+                       {0, 0, 4, 14, 10, 0, 2, 0, 0},
+                       {0, 0, 0, 0, 0, 2, 0, 1, 6},
+                       {8, 11, 0, 0, 0, 0, 1, 0, 7},
+                       {0, 0, 2, 0, 0, 0, 6, 7, 0}};
+
+    dijkstra(graph, 0);
+
+    return 0;
+}
